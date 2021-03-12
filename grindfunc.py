63520543bb08a28564204df3b781d452c4b8f9dc
racumen <info@racumen.it>
2021-03-12 14:37:53 +0100

    grinderone

---
+++ grindfunc.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 22:49:00 2021
+
+@author: f.romano
+"""
+
+
+# from datetime import datetime
+from binance.client import Client
+import pandas as pd
+import statistics 
+import matplotlib.pyplot as plt
+import sklearn
+from joblib import dump,load
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+
+def calculate_values(samples, variables, values):
+    samples_mean=statistics.mean(samples)
+    #samples_mean=0
+    
+    for v in variables:
+        for i in range(v["n_values"]):
+            values[v["offset_values"]+i]=twodec((statistics.mean(samples[v["offset_samples"]+v["n_samples"]*i:v["offset_samples"]+v["n_samples"]*(i+1)])-samples_mean)/samples_mean)
+    #print(values)
+
+
+def get_target(samples, target_definition):
+    target_samples=[row[1] for row in samples]
+    target_samples=target_samples[-target_definition["samples_to"]:-target_definition["samples_from"]]
+    target_samples.sort(reverse=True)
+    #print(samples[-1])
+    return quarterdec(100*(statistics.mean(target_samples[:target_definition["n_samples"]])-samples[-1][1])/samples[-1][1])
+
+def import_samples(datefrom="2021-01-01", dateto="9999"):
+    df=pd.read_csv("Binance_BTCUSDT_minute13.csv",usecols=[1, 3],parse_dates=[1],skiprows=1)
+    #print(df.head())
+    df=df[df["date"]>datefrom]  
+    df=df[df["date"]<dateto]
+    df["open"] = pd.to_numeric(df["open"], downcast="float")
+    #df["date"] = pd.to_datetime(df["date"])
+    return df
+
+def twodec(x):
+    return round(x*100)/100
+def onedec(x):
+    return round(x*10)/10
+def quarterdec(x):
+    return round(x*4)/4
+
+def preparetrain(df,datefrom,dateto,variables,target):
+    df2=df[df["date"]>datefrom]  
+    df2=df2[df2["date"]<dateto]
+    n_samples=0
+    n_values=0
+    for v in variables:
+        n_samples=n_samples+v["n_samples"]*v["n_values"]
+        n_values=n_values+v["n_values"]
+    X = np.empty((0,n_values), dtype='float')
+    y = np.empty((0,1), dtype='float')
+    
+    values=list(range(n_values))
+
+    h_samples=df2.values.tolist()
+    samples=[]
+    count=0
+    for i in range(n_samples):
+        #print("preshift",h_samples[-1])
+        samples.insert(0,h_samples[-1][1])
+        h_samples.pop(-1)
+    while(len(h_samples)>target["samples_to"]):
+             calculate_values(samples, variables,values)
+             y=np.append(y,get_target(h_samples, target))
+             X=np.append(X,[values], axis=0)
+             samples.pop(-1)
+             samples.insert(0,h_samples[-1][1]) 
+             h_samples.pop(-1)
+             count=count+1
+             if not count%100:
+                 print(count,"shift ",h_samples[-1])
+    return X,y
---
+++ grindone.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 22:49:00 2021
+
+@author: f.romano
+"""
+
+
+# from datetime import datetime
+from binance.client import Client
+import pandas as pd
+import statistics 
+import matplotlib.pyplot as plt
+import sklearn
+from joblib import dump,load
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import grindfunc
+
+
+
+
+           
+variables_definition=[
+    {"name":"minutes","n_samples":1,"n_values":300},
+    {"name":"15minutes","n_samples":15,"n_values":20},
+    {"name":"hours","n_samples":60,"n_values":5},
+    {"name":"6hours","n_samples":360,"n_values":5},
+    ]
+
+target_definition={"samples_from":30, "samples_to":120, "n_samples":10}
+
+n_samples=0
+n_values=0
+for v in variables_definition:
+    v["offset_values"]=n_values
+    v["offset_samples"]=n_samples
+    n_samples=n_samples+v["n_samples"]*v["n_values"]
+    n_values=n_values+v["n_values"]
+    
+values=list(range(n_values))
+df=grindfunc.import_samples("2021-01-01")
+
+h_samples=df.values.tolist()
+print("Importati ",len(h_samples),"valori da",h_samples[-1],"a",h_samples[0])
+print("Variabili",n_values)
+print("Giorni",grindfunc.twodec(n_samples/60/24))
+print("Vampioni",n_samples)
+inpt=input("Prepare to train model? y/n")
+if inpt =="y":
+    datefrom="2021-01-01"
+    dateto="2021-03-01"
+    train_X, train_y = grindfunc.preparetrain(df,datefrom,dateto,variables_definition,target_definition)
+    np.savetxt("train_y.csv", train_y, delimiter=",")
+    np.savetxt("train_X.csv", train_X, delimiter=",")
+    
+
+inpt=input("Train model? y/n")
+if inpt =="y":
+    # Instantiate model with 1000 decision trees
+    rf = RandomForestRegressor(n_estimators = 100, random_state = 42)
+    # Train the model on training data
+    rf.fit(train_X, train_y);
+    dump(rf,"rf2.joblib")
+    
+
